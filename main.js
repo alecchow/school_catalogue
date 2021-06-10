d77a78fe7dbc431116b1359acbb94f8d15d239bc
alecchow <84516588+alecchow@users.noreply.github.com>
2021-06-10 19:03:43 +0100

    Add getter for School

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ class School {
       this._level = level;
       this._numOfStudents = numOfStudents;
     }
+
+    get name() {
+        return this._name;
+      }
+  
+      get level() {
+        return this._level;
+      }
+  
+      get numOfStudents() {
+        return this._numOfStudents;
+      }
